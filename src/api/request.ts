2c540747e7906e3b2de091bb1145e0c68d77a742
Absinthe <1198727859@qq.com>
2022-08-10 16:24:56 +0800

    fix: 修复一个问题

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,34 +1,38 @@
-import { ApolloQueryResult } from "@apollo/client";
+import { ApolloQueryResult, OperationVariables } from "@apollo/client";
 import { useMutation, useQuery } from "@vue/apollo-composable";
-import {
-  DocumentParameter,
-  OptionsParameter,
-} from "@vue/apollo-composable/dist/useQuery";
+import { DocumentParameter, OptionsParameter } from "@vue/apollo-composable/dist/useQuery";
 import { DocumentNode } from "graphql";
 
-export const mutation = <T = any, V = any>(gql: DocumentParameter<T, V>) => {
-  const { mutate } = useMutation(gql, {
-    errorPolicy: "all",
-    fetchPolicy: "network-only",
-  });
-  return mutate;
+export const mutation = <
+  TResult = any,
+  TVariables extends OperationVariables = OperationVariables
+>(
+  document: DocumentParameter<TResult, TVariables>,
+  options?: OptionsParameter<TResult, TVariables>
+) => {
+  //@ts-ignore
+  const {mutate} = useMutation(document,{
+    errorPolicy: "all",
+    ...options
+  })
+  return mutate
 };
 
 export const query = <R>(
-  gql: DocumentNode,
-  variables: any,
-  options: OptionsParameter<R, null>
+  gql: DocumentNode,
+  variables: any,
+  options?: OptionsParameter<R, null>
 ): Promise<ApolloQueryResult<R>> => {
-  const { onResult, onError } = useQuery<R>(gql, variables, {
-    errorPolicy: "all",
-    ...options,
-  });
-  return new Promise((resolve, reject) => {
-    onResult((res) => {
-      res.data && resolve(res);
-    });
-    onError((error) => {
-      reject(error.message);
-    });
-  });
+  const { onResult, onError } = useQuery<R>(gql, variables, {
+    errorPolicy: "all",
+    ...options,
+  });
+  return new Promise((resolve, reject) => {
+    onResult((res) => {
+      res.data && resolve(res);
+    });
+    onError((error) => {
+      reject(error.message);
+    });
+  });
 };
